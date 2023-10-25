566403de4039899f83b7cba63315eba932fa3686
Nityan101 <nityancse14@gmail.com>
2023-10-25 19:19:01 +0600

    Edge Launch

--- src/main/java/com/webdriverbasic/browser.java
+++ src/main/java/com/webdriverbasic/browser.java
@@ -2,13 +2,32 @@ package com.webdriverbasic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class browser {
+    public static WebDriver driver;
     public static void main(String[] args){
-        WebDriver driver ;
+
+            chrome_launch();
+            msedge_launcher();
+            chrome_close();
+    }
+
+    public static void chrome_launch(){
+
         System.setProperty("webdriver.chrome.driver","./src/main/resources/Drivers/chromedriver.exe");
         driver = new ChromeDriver();
-       // driver.get("https://www.google.com");
+        // driver.get("https://www.google.com");
+    }
+
+    public static void msedge_launcher(){
+
+        System.setProperty("webdriver.msedge.driver","./src/main/resources/Drivers/msedgedriver.exe");
+        driver = new EdgeDriver();
+
+    }
+    public static void chrome_close(){
+
         driver.close();
     }
 }
